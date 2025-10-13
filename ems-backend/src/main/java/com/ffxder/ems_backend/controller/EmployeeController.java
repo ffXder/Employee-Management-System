c3972b95ecd6a061c0b86305eccfe348bcf01536
ffXder <fjfajardo0624@gmail.com>
2025-10-14 03:09:06 +0800

    Implemented deleteEmployee method

--- ems-backend/src/main/java/com/ffxder/ems_backend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/ffxder/ems_backend/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
@@ -14,17 +16,39 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-    // build add employee REST API
+    // build Add Employee REST API
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+
+    // build Get Employee REST API
     @GetMapping("{id}")
-    // build get employee REST API
     public ResponseEntity<EmployeeDto> getEmployeebyId(@PathVariable("id") Long employeeId){
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
+    }
+
+    // build Get All Employee REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    // build Update Employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,
+                                                      @RequestBody EmployeeDto updatedEmployee) {
+        EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    //build Delete Employee REST API
+    public ResponseEntity<String> deleteEmployee(Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee has been deleted");
     }
 }
