a7971cdce9e4092719f0a1e71c092e7a39f9b833
ffXder <fjfajardo0624@gmail.com>
2025-10-29 01:37:38 +0800

    added pageTitle function using useParams to identify the pages

--- frontend/src/components/EmployeeComponent.jsx
+++ frontend/src/components/EmployeeComponent.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
@@ -8,6 +8,7 @@ const EmployeeComponent = () => {
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
 
+    const {id} = useParams();
     const [errors, setErrors] =useState({
         firstName: '',
         lastName: '',
@@ -61,10 +62,22 @@ const EmployeeComponent = () => {
         return valid;
     }
 
+    function pageTitle(){
+        if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+        } else {
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
   return (
     <div className='container'>
+        <br /> <br />
+        <div className='row'>
         <div className='card col-md-6 offset-md-3 offset-md-2'>
-            <h2 className='text-center'>Add Employee</h2>
+            {
+                pageTitle()
+            }
             <div className='card-body'>
                 <form>
                     {/* firstName input */}
@@ -118,6 +131,7 @@ const EmployeeComponent = () => {
                     <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
                 </form>
             </div>
+        </div>
         </div>
     </div>
   )
