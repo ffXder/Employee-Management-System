a3fd70be9d67450fd386f584450b2b81f4b8f490
ffXder <fjfajardo0624@gmail.com>
2025-11-03 00:34:47 +0800

    implemented CSS components

--- frontend/src/components/HeaderComponents.jsx
+++ frontend/src/components/HeaderComponents.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './css/HeaderComponents.css'
 const HeaderComponents = () => {
   return (
     <div>
         <header>
         <nav className='navbar navbar-dark bg-dark'>
-            <a className="navbar-brand" href="#">Employee Managment System</a>
+            <a className="navbar-brand" href={'/'}>Employee Management System</a>
         </nav>
         </header>
     </div>
